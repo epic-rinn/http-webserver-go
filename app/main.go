88bb2aacccba74640a2e641efe58a4132db56381
rinn-dev <mr.htetmyatlinn404@gmail.com>
2025-01-12 08:02:52 +0700

    refactor: incoming req handling

--- app/main.go
+++ app/main.go
@@ -4,10 +4,33 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
+	"sync"
 )
 
 var _ = net.Listen
 var _ = os.Exit
+var wg sync.WaitGroup
+
+func handleConn(c net.Conn) {
+	tempBuf := make([]byte, 4096)
+	var cd strings.Builder
+
+	n, err := c.Read(tempBuf)
+
+	if err != nil {
+		fmt.Println("Error reading from connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	cd.Write(tempBuf[:n])
+	fmt.Println(cd.String())
+
+	c.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	c.Close()
+
+	wg.Done()
+}
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -16,11 +39,19 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	fmt.Println("Server started on port :4221")
+
+	defer l.Close()
+
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		wg.Add(1)
+		go handleConn(c)
+		wg.Wait()
 	}
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	conn.Close()
 }
